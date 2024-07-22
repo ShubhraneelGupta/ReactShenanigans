4027e279588f2401d220f495aa35878c5da1941f
Shubhraneel Gupta <btech10583.22@bitmesra.ac.in>
2024-07-22 16:16:35 +0530

    dodo

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import {React, useRef} from 'react'
 
-const svgBoy = <svg
-version="1.1"
-baseProfile="full"
-width="300"
-height="200"
-xmlns="http://www.w3.org/2000/svg">
-<rect width="100%" height="100%" fill="black" />
-<circle cx="150" cy="100" r="90" fill="blue" />
-</svg>
+const SVG = ({svgref}) => {
+    return <svg
+    ref={svgref}
+    version="1.1"
+    baseProfile="full"
+    width="300"
+    height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="rgb(255, 255, 255)"/>
+    <circle cx="150" cy="100" r="90" fill="rgb(255,0,0)" />
+    </svg>
+}
 
 
 
 const Ground = () => {
-    return svgBoy
+    const ref = useRef(0)
+    return <>
+        <SVG svgref={ref}/>
+        <button type="button"
+        onClick={() => console.log(ref.current.innerHTML)}
+        >Click</button>
+    </>
 
 }
 
